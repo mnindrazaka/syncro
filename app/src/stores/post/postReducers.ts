5dc40731a770143759ca53fa17196ce24673159f
M. Nindra Zaka <mnindrazaka@gmail.com>
2020-05-28 21:59:49 +0700

    feat(app/post): implement post store

---
+++ app/src/stores/post/postReducers.ts
@@ -0,0 +1,103 @@
+import React from "react";
+import { Post, PostAction } from "./postActions";
+import { Action } from "../StoreContext";
+import { combineReducers } from "../combineReducers";
+
+export type PostState = {
+  items: Post[];
+  selected?: Post;
+  loading: boolean;
+  error?: string;
+};
+
+export const postInitialState: PostState = {
+  items: [],
+  selected: undefined,
+  loading: false,
+  error: undefined
+};
+
+const itemsReducer: React.Reducer<PostState["items"], Action> = (
+  prevState,
+  action
+) => {
+  switch (action.type) {
+    case PostAction.GetSuccess:
+      return [...action.payload];
+    case PostAction.CreateSuccess:
+      return [...prevState, action.payload];
+    case PostAction.EditSuccess:
+      return prevState.map(post =>
+        post._id === action.payload._id ? action.payload : post
+      );
+    case PostAction.DeleteSuccess:
+      return prevState.filter(post => post._id !== action.payload._id);
+    default:
+      return prevState;
+  }
+};
+
+const selectedReducer: React.Reducer<PostState["selected"], Action> = (
+  prevState,
+  action
+) => {
+  switch (action.type) {
+    case PostAction.Select:
+      return action.payload;
+    case PostAction.ClearSelected:
+      return undefined;
+    default:
+      return prevState;
+  }
+};
+
+const loadingReducer: React.Reducer<PostState["loading"], Action> = (
+  prevState,
+  action
+) => {
+  switch (action.type) {
+    case PostAction.GetRequest:
+    case PostAction.CreateRequest:
+    case PostAction.EditRequest:
+    case PostAction.DeleteRequest:
+      return true;
+    case PostAction.GetSuccess:
+    case PostAction.GetError:
+    case PostAction.CreateSuccess:
+    case PostAction.CreateError:
+    case PostAction.EditSuccess:
+    case PostAction.EditError:
+    case PostAction.DeleteSuccess:
+    case PostAction.DeleteError:
+      return false;
+    default:
+      return prevState;
+  }
+};
+
+const errorReducer: React.Reducer<PostState["error"], Action> = (
+  prevState,
+  action
+) => {
+  switch (action.type) {
+    case PostAction.GetError:
+    case PostAction.CreateError:
+    case PostAction.EditError:
+    case PostAction.DeleteError:
+      return action.payload;
+    case PostAction.GetSuccess:
+    case PostAction.CreateSuccess:
+    case PostAction.EditSuccess:
+    case PostAction.DeleteSuccess:
+      return undefined;
+    default:
+      return prevState;
+  }
+};
+
+export default combineReducers<PostState>({
+  items: [itemsReducer, postInitialState["items"]],
+  selected: [selectedReducer, postInitialState["selected"]],
+  loading: [loadingReducer, postInitialState["loading"]],
+  error: [errorReducer, postInitialState["error"]]
+});
---
+++ app/src/stores/post/usePostAction.ts
@@ -0,0 +1,119 @@
+import React from "react";
+import { useStoreContext } from "../StoreContext";
+import {
+  Post,
+  getRequestAction,
+  getSuccessAction,
+  getErrorAction,
+  createRequestAction,
+  createSuccessAction,
+  createErrorAction,
+  editRequestAction,
+  editSuccessAction,
+  editErrorAction,
+  deleteRequestAction,
+  deleteSuccessAction,
+  deleteErrorAction,
+  selectAction,
+  clearSelectedAction
+} from "./postActions";
+
+const usePostActions = () => {
+  const storeContext = useStoreContext();
+
+  const getRequest = React.useCallback(() => {
+    storeContext.dispatch(getRequestAction());
+  }, [storeContext]);
+  const getSuccess = React.useCallback(
+    (posts: Post[]) => {
+      storeContext.dispatch(getSuccessAction(posts));
+    },
+    [storeContext]
+  );
+  const getError = React.useCallback(
+    (message: string) => {
+      storeContext.dispatch(getErrorAction(message));
+    },
+    [storeContext]
+  );
+
+  const createRequest = React.useCallback(() => {
+    storeContext.dispatch(createRequestAction());
+  }, [storeContext]);
+  const createSuccess = React.useCallback(
+    (post: Post) => {
+      storeContext.dispatch(createSuccessAction(post));
+    },
+    [storeContext]
+  );
+  const createError = React.useCallback(
+    (message: string) => {
+      storeContext.dispatch(createErrorAction(message));
+    },
+    [storeContext]
+  );
+
+  const editRequest = React.useCallback(() => {
+    storeContext.dispatch(editRequestAction());
+  }, [storeContext]);
+  const editSuccess = React.useCallback(
+    (post: Post) => {
+      storeContext.dispatch(editSuccessAction(post));
+    },
+    [storeContext]
+  );
+  const editError = React.useCallback(
+    (message: string) => {
+      storeContext.dispatch(editErrorAction(message));
+    },
+    [storeContext]
+  );
+
+  const deleteRequest = React.useCallback(() => {
+    storeContext.dispatch(deleteRequestAction());
+  }, [storeContext]);
+  const deleteSuccess = React.useCallback(
+    (post: Post) => {
+      storeContext.dispatch(deleteSuccessAction(post));
+    },
+    [storeContext]
+  );
+  const deleteError = React.useCallback(
+    (message: string) => {
+      storeContext.dispatch(deleteErrorAction(message));
+    },
+    [storeContext]
+  );
+
+  const select = React.useCallback(
+    (post: Post) => {
+      storeContext.dispatch(selectAction(post));
+    },
+    [storeContext]
+  );
+  const clearSelected = React.useCallback(
+    (post: Post) => {
+      storeContext.dispatch(clearSelectedAction());
+    },
+    [storeContext]
+  );
+
+  return {
+    getRequest,
+    getSuccess,
+    getError,
+    createRequest,
+    createSuccess,
+    createError,
+    editRequest,
+    editSuccess,
+    editError,
+    deleteRequest,
+    deleteSuccess,
+    deleteError,
+    select,
+    clearSelected
+  };
+};
+
+export default usePostActions;
