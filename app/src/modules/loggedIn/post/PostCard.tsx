ceb5cb07fcfd62706e60c5d494664a203af25c1f
M Nindra Zaka <mnindrazaka@gmail.com>
2020-06-01 07:09:08 +0700

    feat(app/post): show name and date in post card

--- app/src/modules/loggedIn/post/PostCard.tsx
+++ app/src/modules/loggedIn/post/PostCard.tsx
@@ -1,11 +1,35 @@
 import React from "react";
 import { View } from "react-native";
 import { Post } from "../../../stores/post/postActions";
-import { Text } from "@ui-kitten/components";
+import { Text, Icon } from "@ui-kitten/components";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
 const PostCard = (props: { post: Post }) => {
+  const isSameYear = dayjs().isSame(props.post.createdAt, "year");
+  const isSameDay = dayjs().isSame(props.post.createdAt, "day");
+
   return (
-    <View style={{ padding: 8, backgroundColor: "#fff" }}>
+    <View style={{ padding: 16, backgroundColor: "#fff" }}>
+      <View style={{ flexDirection: "row", marginBottom: 16 }}>
+        <Icon
+          name="person-outline"
+          fill="#000"
+          style={{ height: 40, width: 40 }}
+        />
+        <View>
+          <Text>{props.post.user.name}</Text>
+          <Text category="c1" appearance="hint">
+            {isSameYear
+              ? isSameDay
+                ? dayjs(props.post.createdAt).fromNow()
+                : dayjs(props.post.createdAt).format("MMMM DD [at] hh:mm A")
+              : dayjs(props.post.createdAt).format("MMMM DD, YYYY")}
+          </Text>
+        </View>
+      </View>
+
       <Text>{props.post.content}</Text>
     </View>
   );
--- app/src/modules/loggedIn/post/PostList.tsx
+++ app/src/modules/loggedIn/post/PostList.tsx
@@ -2,40 +2,42 @@ import React from "react";
 import usePostState from "../../../stores/post/usePostState";
 import usePostAction from "../../../stores/post/usePostAction";
 import sosmedService from "../../../utils/api/sosmedService";
-import { Alert, View, ScrollView } from "react-native";
+import { Alert, View, ScrollView, RefreshControl } from "react-native";
 import PostCard from "./PostCard";
-import { Spinner } from "@ui-kitten/components";
 
 const PostList = () => {
   const postState = usePostState();
   const postAction = usePostAction();
 
+  const handleGetPost = React.useCallback(async () => {
+    try {
+      postAction.getRequest();
+      const posts = await sosmedService.getAllPosts();
+      postAction.getSuccess(posts);
+    } catch (err) {
+      postAction.getError(err.message);
+      Alert.alert("Unable to fetch post", err.message);
+    }
+  }, [postAction]);
+
   React.useEffect(() => {
-    (async () => {
-      try {
-        postAction.getRequest();
-        const posts = await sosmedService.getAllPosts();
-        postAction.getSuccess(posts);
-      } catch (err) {
-        postAction.getError(err.message);
-        Alert.alert("Unable to fetch post", err.message);
-      }
-    })();
+    handleGetPost();
   }, []);
 
   return (
-    <ScrollView>
-      {postState.loading ? (
-        <View style={{ alignItems: "center" }}>
-          <Spinner />
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          refreshing={postState.loading}
+          onRefresh={handleGetPost}
+        />
+      }
+    >
+      {postState.items.reverse().map(item => (
+        <View style={{ marginBottom: 8 }} key={item._id}>
+          <PostCard post={item} />
         </View>
-      ) : (
-        postState.items.map(item => (
-          <View style={{ marginBottom: 8 }} key={item._id}>
-            <PostCard post={item} />
-          </View>
-        ))
-      )}
+      ))}
     </ScrollView>
   );
 };
